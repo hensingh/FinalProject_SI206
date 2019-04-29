2c10a5fea1885cbe3fc7fcb350b651466896fcf8
Andrew Kaiser <arkaiser@umich.edu>
2019-04-29 16:38:11 -0400

    finalized

--- NYapi.py
+++ NYapi.py
@@ -22,45 +22,35 @@ def nyUS():
     ny = requests.get('https://api.nytimes.com/svc/news/v3/content/nyt/U.S./168.json?limit=20&api-key=YG8Tktps3JKniMJohga7kO5f5WecrgW6')
     ny_obj = json.loads(ny.text)
 
-    nyUS_list = []
     for item in ny_obj['results']:
-        nyUS_list.append(len(item['title']))
         cur.execute('INSERT INTO nytimes (section, length) VALUES(?,?)', (item['section'], len(item['title'])))
 
 def nyWORLD():
     ny = requests.get('https://api.nytimes.com/svc/news/v3/content/nyt/World/168.json?limit=20&api-key=YG8Tktps3JKniMJohga7kO5f5WecrgW6')
     ny_obj = json.loads(ny.text)
 
-    nyWORLD_list = []
     for item in ny_obj['results']:
-        nyWORLD_list.append(len(item['title']))
         cur.execute('INSERT INTO nytimes (section, length) VALUES(?,?)', (item['section'], len(item['title'])))
 
 def nySPORTS():
     ny = requests.get('https://api.nytimes.com/svc/news/v3/content/nyt/Sports/168.json?limit=20&api-key=YG8Tktps3JKniMJohga7kO5f5WecrgW6')
     ny_obj = json.loads(ny.text)
 
-    nySPORTS_list = []
     for item in ny_obj['results']:
-        nySPORTS_list.append(len(item['title']))
         cur.execute('INSERT INTO nytimes (section, length) VALUES(?,?)', (item['section'], len(item['title'])))
 
 def nyBUSINESS():
     ny = requests.get('https://api.nytimes.com/svc/news/v3/content/nyt/Business/168.json?limit=20&api-key=YG8Tktps3JKniMJohga7kO5f5WecrgW6')
     ny_obj = json.loads(ny.text)
 
-    nyBUSINESS_list = []
     for item in ny_obj['results']:
-        nyBUSINESS_list.append(len(item['title']))
         cur.execute('INSERT INTO nytimes (section, length) VALUES(?,?)', (item['section'], len(item['title'])))
 
 def nyBOOKS():
     ny = requests.get('https://api.nytimes.com/svc/news/v3/content/nyt/Books/168.json?limit=20&api-key=YG8Tktps3JKniMJohga7kO5f5WecrgW6')
     ny_obj = json.loads(ny.text)
 
-    nyNY_list = []
     for item in ny_obj['results']:
-        nyNY_list.append(len(item['title']))
         cur.execute('INSERT INTO nytimes (section, length) VALUES(?,?)', (item['section'], len(item['title'])))
      
 #cur.close()
